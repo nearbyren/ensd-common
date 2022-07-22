cb58e67083d8c514c21eba1de5be617683988f5f
lr <wangcuhnrong@ejiayou.com>
2022-07-22 14:18:05 +0800

    ...

--- common-module/src/main/java/ejiayou/common/module/dto/MyBannerBean.kt
+++
@@ -1,8 +0,0 @@
-package ejiayou.common.module.dto
-
-/**
- * @author:
- * @created on: 2022/7/11 13:50
- * @description:
- */
-data class MyBannerBean<T>(var success: String, var status: String, var tip: String, var data: BannerBean)
--- common-module/src/main/java/ejiayou/common/module/dto/TestBannerBean.kt
+++ common-module/src/main/java/ejiayou/common/module/dto/TestBannerBean.kt
@@ -5,6 +5,6 @@ package ejiayou.common.module.dto
  * @created on: 2022/7/11 13:50
  * @description:
  */
-data class BannerBean(val url: String) {
+data class TestBannerBean(val url: String) {
 
 }
